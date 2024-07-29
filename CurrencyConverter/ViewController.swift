cec9a43add6d4cb2d3e7682faeb1a9bf38c4ac3c
Mark Santoro <mark_santoro@comcast.net>
2024-07-29 13:05:15 -0700

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Mark Santoro on 7/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
